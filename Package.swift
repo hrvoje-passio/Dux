c9668f08572dce88cb2a754b3d320d2885408e26
hrvoje-passio <87117274+hrvoje-passio@users.noreply.github.com>
2022-10-07 14:00:08 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Dux",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v13_4),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
